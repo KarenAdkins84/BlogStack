732473900b4e8d16e5263b714607e176a1885e91
Nerakma84 <fnk8476@gmail.com>
2023-03-21 13:20:58 -0500

    added session and auth

--- controllers/users/users.js
+++ controllers/users/users.js
@@ -1,9 +1,33 @@
+const bcrypt = require("bcryptjs");
+const User = require("../../model/user/User");
+const appErr = require("../../utils/appErr");
+
 //register
-const registerCtrl = async(req, res)=>{
+const registerCtrl = async(req, res, next)=>{
+    const { fullname, email, password } = req.body;
+    //check if field is empty
+    if(!fullname || !email || !password){
+        return next(appErr('All fields are required'))
+    }
     try {
+        //check if user exists by email
+        const userFound = await User.findOne({ email });
+        //throw an error
+        if(userFound){
+            return next(appErr('User alresady exists'));
+        }
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const passwordHashed = await bcrypt.hash(password, salt)
+        //register user
+        const user = await User.create({
+            fullname, 
+            email, 
+            password: passwordHashed,
+        });
         res.json({
             status: 'success',
-            user: 'User registered'
+            data: user,
         });
     } catch (error) {
         res.json(error);
@@ -11,11 +35,30 @@ const registerCtrl = async(req, res)=>{
 };
 
 //login
-const loginCtrl = async(req, res)=>{
+const loginCtrl = async(req, res, next)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return next(appErr('Email and password fields are required'));
+    }
     try {
+        //check if email exists
+        const userFound = await User.findOne({ email });
+        if(!userFound) {
+            //throw an error
+            return next(appErr('Invalid login credentials'));
+        }
+        //verify password
+        const isPasswordValid = await bcrypt.compare(password, userFound.password);
+        if(!isPasswordValid){
+        //throw an error
+        return next(appErr('Invalid login credentials'));
+        }
+        //save the user into session
+        req.session.userAuth = userFound._id;
+        console.log(req.session)
         res.json({
             status: 'success',
-            user: 'User login'
+            data: userFound,
         });
     } catch (error) {
         res.json(error);
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+const globalErrHandler = require('./middlewares/globalHandler');
 const userRoutes = require('./routes/users/users');
 const postRoutes = require('./routes/posts/posts');
 const commentRoutes = require('./routes/comments/comment');
 
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
-// const globalErrHandler = require('./middlewares/globalHandler');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 require('./config/dbConnect');
-
-
 
 const app = express();
 //middlewares
 //-------
+app.use(express.json()); //parse incoming data
 
+//session config
+app.use(
+    session({
+        secret: process.env.SESSION_KEY,
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({
+            mongoUrl: process.env.MONGO_URL,
+            ttl: 24 * 60 * 60, //1 day-session storage
+        }),
+    })
+);
 //users route base url
 ///api/v1/users
 app.use('/api/v1/users', userRoutes);
@@ -29,7 +40,7 @@ app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/comments', commentRoutes);
 
 //error handler middlewares
-
+app.use(globalErrHandler);
 //listen server
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT} `));
