74a8faa4e0724245364af2708a111bcf0b2ff007
Nerakma84 <fnk8476@gmail.com>
2023-04-21 12:42:08 -0500

    deploying

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnect = async () => {
     console.log(process.env);
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/blog-stack');
         console.log('DB Connected Successfully');
     } catch (error) {
         console.log('DB Connection Failed', error.message);
